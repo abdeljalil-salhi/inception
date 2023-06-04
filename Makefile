28933d90f3d7977104266c7c110ddc8976564f22
Abdel <abdelisloading@gmail.com>
2023-06-04 13:32:52 -0500

    Setup nginx and wordpress

--- Makefile
+++ Makefile
@@ -1,19 +1,31 @@
-all	:	re
+all		:	re
 
-up	:
-		@docker-compose -f srcs/docker-compose.yml up --build -d
+up		:	chown
+			@mkdir -p /home/absalhi/volumes/wordpress/ /home/absalhi/volumes/mariadb/
+			@docker-compose -f srcs/docker-compose.yml up --build -d
 
 down	:
-		@docker-compose -f srcs/docker-compose.yml down
+			@docker-compose -f srcs/docker-compose.yml down
 
 clean	:	down
-		@rm -rf volumes/mariadb/*
-		@docker-compose -f srcs/docker-compose.yml rm -v
+			@rm -rf /home/absalhi/volumes/
+			@docker-compose -f srcs/docker-compose.yml rm -v
 
 fclean	:	clean
-		@docker system prune -f
+			@docker network prune -f
+			@docker system prune -f
 
-re	:	fclean up
+re		:	fclean up
 
-.PHONY	:	all up down clean fclean re
+clear_volumes	:
+			@docker volume rm mariadb wordpress
 
+chown	:
+			@chown -R absalhi:absalhi ./*
+			@chown -R absalhi:absalhi ./.*
+
+nginx	:
+			@docker build -t nginx ./srcs/requirments/nginx/
+			@docker run -it --rm -p 80:80 -p 443:443 nginx -e ALPINE_VERSION=3.18.0
+
+.PHONY	:	all up down clean fclean clear_volumes re chown nginx
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
-
-while ! mariadb -h$MARIADB_HOSTNAME -u$MARIADB_USERNAME -p$MARIADB_PASSWORD $MARIADB_DATABASE &>/dev/null; do
-    sleep 3
-done
-
-if [ ! -f "/var/www/html/index.html" ]; then
-
-    mv /tmp/index.html /var/www/html/index.html
-
-    wp core download --allow-root
-    wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USERNAME --dbpass=$MARIADB_PASSWORD --dbhost=$MARIADB_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-    wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USERNAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-    wp user create $WP_USERNAME $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
-    wp theme install inspiro --activate --allow-root
-    wp plugin update --all --allow-root
-
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
+	echo "downloading wordpress..."
+	wp core download --allow-root
+	sed -i "s/username_here/$MARIADB_USERNAME/g" wp-config-sample.php
+	sed -i "s/password_here/$MARIADB_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$MARIADB_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MARIADB_DATABASE/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
+	wp core install --allow-root --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USERNAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
+	wp user create --allow-root $WP_USERNAME $WP_EMAIL --user_pass=$WP_PASSWORD --role=author
+	wp theme install --allow-root inspiro --activate
 fi
-
-
 echo "wordpress listening on port 9000"
-/usr/sbin/php-fpm7 -F -R
+exec "$@"
