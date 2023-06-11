ba36a9d0ba8567e4724f7b786f6af3b8ce7e7b0a
Abdel <abdelisloading@gmail.com>
2023-06-11 02:33:48 -0500

    Fix MariaDB ALTER bug and convert to Alpine

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
-service mysql start
-mariadb -u root -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DB}\`;"
-mariadb -u root -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PWD}';"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DB}\`.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PWD}';"
-mariadb -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PWD}';"
-mariadb -u root -e "FLUSH PRIVILEGES;"
+if [ ! -d "/run/mysqld" ]; then
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	chown -R mysql:mysql /var/lib/mysql
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+	if [ ! -f "/tmp/query.sql" ]; then
+		return 1
+	fi
+	cat << EOF > /tmp/query.sql
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM	mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PWD';
+CREATE DATABASE $MARIADB_DB CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PWD';
+GRANT ALL PRIVILEGES ON $MARIADB_DB.* TO '$MARIADB_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+	mysqld --user=mysql --bootstrap < /tmp/query.sql
+	rm -f /tmp/query.sql
+fi
+exec mysqld --user=mysql --console
